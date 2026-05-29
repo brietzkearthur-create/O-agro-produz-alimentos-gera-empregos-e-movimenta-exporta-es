cb34813d9f6341bff878e570653e0bb6ce731394
brietzkearthur-create <brietzke.arthur@escola.pr.gov.br>
2026-05-29 11:44:08 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarMensagem() {
+
+    const mensagem = document.getElementById("mensagem");
+
+    mensagem.innerHTML =
+    "🌾 O agro é essencial para alimentar pessoas, gerar empregos e fortalecer a economia brasileira!";
+
+}
